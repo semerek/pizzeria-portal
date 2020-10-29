e352f6a9c8b0658aecf26fce1d844abbeec3df9b
Marta Semrau <mm.semrau@gmail.com>
2020-10-29 11:21:08 +0100

    18.4 Implementation of Material UI

--- src/App.js
+++ src/App.js
@@ -12,26 +12,30 @@ import EventsBooked from './components/views/EventsBooked/EventsBooked';
 import EventsBookedNew from './components/views/EventsBookedNew/EventsBookedNew';
 import WaiterOrder from './components/views/WaiterOrder/WaiterOrder';
 import WaiterOrderNew from './components/views/WaiterOrderNew/WaiterOrderNew';
+import { StylesProvider } from '@material-ui/core/styles';
+
 
 
 function App() {
   return (
     <BrowserRouter basename={'/panel'}>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
-          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />  
-          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />  
-          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />  
-          <Route exact path={process.env.PUBLIC_URL + '/tables/booked/:id'} component={TablesBooked} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={TablesBookedNew} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={EventsBooked} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={EventsBookedNew} />
-          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} /> 
-          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={WaiterOrder} />
-          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={WaiterOrderNew} />
-        </Switch>
-      </MainLayout>
+      <StylesProvider injectFirst>
+        <MainLayout>
+          <Switch>
+            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+            <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />  
+            <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />  
+            <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />  
+            <Route exact path={process.env.PUBLIC_URL + '/tables/booked/:id'} component={TablesBooked} />
+            <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={TablesBookedNew} />
+            <Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={EventsBooked} />
+            <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={EventsBookedNew} />
+            <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} /> 
+            <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={WaiterOrder} />
+            <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={WaiterOrderNew} />
+          </Switch>
+        </MainLayout>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import styles from './PageNav.module.scss';
+import Button from '@material-ui/core/Button';
+
 
 
 
@@ -8,11 +10,11 @@ import styles from './PageNav.module.scss';
 function PageNav() {
   return (
     <nav className={styles.component}>
-      <NavLink exact to={`${process.env.PUBLIC_URL}/`} className={styles.link} activeClassName='active'>Home</NavLink>
-      <NavLink to={`${process.env.PUBLIC_URL}/login`} className={styles.link} activeClassName='active'>Login</NavLink>
-      <NavLink to={`${process.env.PUBLIC_URL}/kitchen`} className={styles.link} activeClassName='active'>Kitchen</NavLink>
-      <NavLink to={`${process.env.PUBLIC_URL}/tables`} className={styles.link} activeClassName='active'>Tables</NavLink>
-      <NavLink to={`${process.env.PUBLIC_URL}/waiter`} className={styles.link} activeClassName='active'>Waiter</NavLink>
+      <Button component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} className={styles.link} activeClassName='active'>Home</Button>
+      <Button component={NavLink} to={`${process.env.PUBLIC_URL}/login`} className={styles.link} activeClassName='active'>Login</Button>
+      <Button component={NavLink} to={`${process.env.PUBLIC_URL}/kitchen`} className={styles.link} activeClassName='active'>Kitchen</Button>
+      <Button component={NavLink} to={`${process.env.PUBLIC_URL}/tables`} className={styles.link} activeClassName='active'>Tables</Button>
+      <Button component={NavLink} to={`${process.env.PUBLIC_URL}/waiter`} className={styles.link} activeClassName='active'>Waiter</Button>
     </nav>
   );
 }
